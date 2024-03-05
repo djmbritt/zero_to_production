8e0e9b3f58f8efffda54ae7bc90b98c418b05c76
David Britt <djmb102@gmail.com>
2024-03-05 18:20:18 -0400

    Use lazy connect for database

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero_to_prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
 
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     let _ = startup::run(listener, connection_pool)?.await;
