d440966dbd734d075b65a8acde5a4721904e06ac
David Britt <djmb102@gmail.com>
2024-03-11 23:41:00 -0400

    Split up domain, add subscriber info

---
+++ src/domain/subscriber_name.rs
@@ -0,0 +1,87 @@
+// This provides the graphemes method to parse each character as a grapheme
+// Graphemes are a compound of multiple characters and interpreted as one character by humans
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct NewSubscriber {
+    pub email: String,
+    pub name: SubscriberName,
+}
+
+#[derive(Debug)]
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    /// parse function allows us to create an instance of SubscriberName while making sure
+    /// the instance conforms to the rules of what a SubscriberName should look like
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
+        let is_empty_or_whitespace = s.trim().is_empty();
+
+        let is_too_long = s.graphemes(true).count() > 256;
+
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s.chars().any(|c| forbidden_characters.contains(&c));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            // The type supplied to Err() will dermine the type of Err()
+            // This type needs to correspond with the function signature.
+            Err(format!(
+                "Supplied name is not a valid subscriber name: {}",
+                s
+            ))
+        } else {
+            Ok(Self(s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberName {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    const MAX_LENGTH: usize = 256;
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "Å".repeat(MAX_LENGTH);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(MAX_LENGTH + 1);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_succesfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+}
